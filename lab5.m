f921b241b37ad9d7386bf83ec5c30d8662ef22ae
atoniolin <atoniolin@yahoo.com.my>
2017-03-02 15:56:26 +0000

    Update lab5.m

--- lab5.m
+++ lab5.m
@@ -1,7 +1,7 @@
 clear all;
 close all;
 
-for order=6:6
+for order=1:6
 [b,a] = ellip(order,0.3,20,[200 450]/4000);
 
 figure;
@@ -16,7 +16,7 @@ title('Pole-Zero Plot for Lab 5 IIR Bandpass Filter');
 grid on;
 
 %IIR coefficients output printed according to C-syntax
-str = sprintf('IIR_coeff_DF2T_%d.txt',order*2);
+str = sprintf('IIR_coeff_%d.txt',order*2);
 fid = fopen(str,'w');
 %fprintf(fid, '#define N %d\n', length(a));
 %fprintf(fid, '#define M %d\n', length(b));
